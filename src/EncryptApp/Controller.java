a9d91a426cdc44f120478ca9e1aa22eccd502713
Dobrucki <dobrucki2@gmail.com>
2018-11-04 17:17:42 +0100

    finished gui, need binding to alhorithm

--- src/EncryptApp/Controller.java
+++ src/EncryptApp/Controller.java
@@ -1,4 +1,34 @@
 package EncryptApp;
 
+import com.sun.istack.internal.NotNull;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.layout.VBox;
+
 public class Controller {
+    @FXML
+    private VBox fileVBox;
+    @FXML
+    private VBox textVBox;
+    @FXML
+    private RadioButton textViewSelected;
+    @FXML
+    private RadioButton fileViewSelected;
+
+    @FXML
+    private void toggleView(@NotNull ActionEvent event){
+        if(event.getSource().equals(fileViewSelected)){
+           fileVBox.setDisable(false);
+           fileVBox.setVisible(true);
+           textVBox.setDisable(true);
+           textVBox.setVisible(false);
+        }
+        else if(event.getSource().equals(textViewSelected)){
+            fileVBox.setDisable(true);
+            fileVBox.setVisible(false);
+            textVBox.setDisable(false);
+            textVBox.setVisible(true);
+        }
+    }
 }
--- src/EncryptApp/EncryptApp.java
+++ src/EncryptApp/EncryptApp.java
@@ -2,20 +2,20 @@ package EncryptApp;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class EncryptApp extends Application {
 
+    private Stage stage;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent textScene = FXMLLoader.load(getClass().getResource("textScene.fxml"));
-        primaryStage.setTitle("Program szyfrujący");
-        primaryStage.setScene(new Scene(textScene));
+        stage = primaryStage;
+        stage.setTitle("Program szyfrujący");
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("gui.fxml"))));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
